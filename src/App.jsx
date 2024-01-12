4a9265b09275caae297f94803c14a419e02b0032
aahrom <ahromarreya@gmail.com>
2024-01-12 13:24:14 -0600

    Cleaned up final product

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,6 @@ function App() {
     <div className="main-card">
     <Header />
     <Main />
-    <Footer />
     </div>
   )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,7 @@
 import React from "react"
 import portrait from "../assets/portrait.png"
+import MailOutlineIcon from '@mui/icons-material/MailOutline';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
 
 export default function Header(){
 
@@ -9,8 +11,8 @@ return (
         <h3 className="name">Arreya Ahrom</h3>
         <p className="description">Frontend Developer</p>
         <div className="socials">
-        <button>Email</button>
-        <button>LinkedIn</button>
+        <button className="email--button"><MailOutlineIcon/><a href="mailto:ahromarreya@gmail.com"  target="_blank">Email</a></button>
+        <button className="linkedin--button"><LinkedInIcon/><a href="https://www.linkedin.com/in/arreyaahrom/" target="_blank">LinkedIn</a></button>
         </div>
     </div>
 
